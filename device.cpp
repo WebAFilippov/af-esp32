187366e852232b6cea034e2224c66dd8901efd40
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2024-12-29 06:54:47 +0700

    refactor:: att FreeRToS

--- device.cpp
+++ device.cpp
@@ -8,17 +8,15 @@
 #include "saved_html.h"
 
 // Пины
-#define LED_PIN 2  // Встроенный светодиод(синий)
-
-#define CLK 22  // Encoder left
-#define DT 23   // Encoder right
-#define SW 21   // Кнопка Енкодер
+#define LED_PIN 2
+#define CLK 22
+#define DT 23
+#define SW 21
 
 #define MANUAL 0
 #define AUTO 1
 
-// Состояния
-bool buttonPressed = false;
+// Переменные состояния
 bool serverMode = false;
 
 // Объекты
@@ -27,10 +25,6 @@ Preferences preferences;
 WiFiClient espClient;
 PubSubClient client(espClient);
 Encoder encoder(CLK, DT, SW);
-
-// Таймеры
-unsigned long buttonPressStart = 0;
-const unsigned long longPressDuration = 5000;  // 5 секунд
 
 // MQTT
 const int mqtt_port = 1883;
@@ -42,13 +36,19 @@ String savedSSID;
 String savedPassword;
 String savedMqttServer;
 
+// Статические IP-адреса для AP Mode
+IPAddress local_IP(192, 168, 4, 100);
+IPAddress gateway(192, 168, 4, 100);
+IPAddress subnet(255, 255, 255, 0);
+
+// Функция отправки MQTT-команды
 void sendMqttCommand(const char* topic, const char* message) {
   if (client.connected()) {
     client.publish(topic, message);
   }
 }
 
-
+// MQTT коллбэк
 void callback(char* topic, byte* message, unsigned int length) {
   String messageTemp;
   for (int i = 0; i < length; i++) {
@@ -60,6 +60,7 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.println(messageTemp);
 }
 
+// Обработчики сервера
 void handleRoot() {
   server.send(200, "text/html", index_html);
 }
@@ -69,120 +70,103 @@ void handleSave() {
   String password = server.arg("password");
   String mqttServer = server.arg("mqtt_server");
 
-  Serial.println("Saving Wi-Fi and MQTT settings:");
-  Serial.print("SSID: ");
-  Serial.println(ssid);
-  Serial.print("Password: ");
-  Serial.println(password);
-  Serial.print("MQTT Server: ");
-  Serial.println(mqttServer);
-
-  if (preferences.getString("ssid") != ssid) preferences.putString("ssid", ssid);
-  if (preferences.getString("password") != password) preferences.putString("password", password);
-  if (preferences.getString("mqtt_server") != mqttServer) preferences.putString("mqtt_server", mqttServer);
+  preferences.putString("ssid", ssid);
+  preferences.putString("password", password);
+  preferences.putString("mqtt_server", mqttServer);
 
   server.send(200, "text/html", saved_html);
   delay(1000);
   ESP.restart();
 }
 
-bool isLongPress() {
-  static bool buttonPressed = false;
-  static unsigned long pressStart = 0;
-
-  if (digitalRead(SW) == LOW) {
-    if (!buttonPressed) {
-      buttonPressed = true;
-      pressStart = millis();
-    } else if (millis() - pressStart >= longPressDuration) {
-      return true;  // Длинное нажатие
+// Задачи FreeRTOS
+void TaskWiFiReconnect(void* pvParameters) {
+  while (1) {
+    if (WiFi.status() != WL_CONNECTED && !serverMode) {
+      Serial.println("Reconnecting WiFi...");
+      WiFi.begin(savedSSID.c_str(), savedPassword.c_str());
+      delay(5000);
     }
-  } else {
-    buttonPressed = false;
-  }
-  return false;
-}
-
-void handleButtonPress() {
-  if (isLongPress()) {
-    resetWiFiSettings();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
-void connectToWiFi(const char* ssid, const char* password) {
-  Serial.print("Connecting to WiFi: ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-  unsigned long startAttemptTime = millis();
-
-  while (WiFi.status() != WL_CONNECTED) {
-    // Проверка на превышение времени ожидания (10 секунд)
-    if (millis() - startAttemptTime > 10000) {
-      Serial.println("WiFi connection failed!");
-      updateLED();
-      return;
-    }
-
-    // Проверка долгого нажатия кнопки
-    handleButtonPress();
-    delay(100);  // Короткая задержка для экономии ресурсов
-  }
-
-  Serial.println("WiFi connected!");
-}
-
-void reconnectWiFi() {
-  unsigned long startAttemptTime = millis();
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Reconnecting to WiFi...");
-    WiFi.begin(savedSSID.c_str(), savedPassword.c_str());
-
-    unsigned long currentTime = millis();
-    if (currentTime - startAttemptTime > 10000) {  // 10 секунд таймаут
-      Serial.println("WiFi connection failed!");
-      updateLED();
-      return;
-    }
-
-    handleButtonPress();
-    delay(500);
-  }
-
-  Serial.println("WiFi reconnected successfully.");
-  updateLED();
-}
-
-void reconnectMQTT() {
-  unsigned long startAttemptTime = millis();
-  while (!client.connected()) {
-    Serial.print("Connecting to MQTT... ");
-    if (client.connect(mqtt_client_id)) {
-      Serial.println("connected!");
-      client.subscribe(mqtt_topic_sub_mask);
-      updateLED();
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again later...");
-
-      unsigned long currentTime = millis();
-      if (currentTime - startAttemptTime > 10000) {
-        Serial.println("MQTT connection failed!");
-        updateLED();
-        return;
+void TaskMQTTReconnect(void* pvParameters) {
+  while (1) {
+    if (!client.connected() && !serverMode) {
+      Serial.print("Connecting to MQTT... ");
+      if (client.connect(mqtt_client_id)) {
+        Serial.println("connected!");
+        client.subscribe(mqtt_topic_sub_mask);
+      } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again later...");
       }
+    }
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
 
-      handleButtonPress();
-      delay(500);
+void TaskEncoder(void* pvParameters) {
+  while (1) {
+    encoder.tick();
+    if (encoder.isRight()) {
+      sendMqttCommand("increment/volume", "+");
+    }
+    if (encoder.isLeft()) {
+      sendMqttCommand("decrement/volume", "-");
+    }
+    if (encoder.isSingle()) {
+      sendMqttCommand("toggle/volume", "toggle");
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskWebServer(void* pvParameters) {
+  while (1) {
+    server.handleClient();
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskLED(void* pvParameters) {
+  while (1) {
+    if (WiFi.status() != WL_CONNECTED) {
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+    } else if (!client.connected()) {
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+    } else {
+      digitalWrite(LED_PIN, HIGH);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
   }
 }
 
-void resetWiFiSettings() {
-  Serial.println("Resetting Wi-Fi settings...");
-  preferences.clear();
-  ESP.restart();
+void TaskButton(void* pvParameters) {
+  while (1) {
+    if (digitalRead(SW) == LOW) {
+      delay(50);
+      unsigned long pressStart = millis();
+      while (digitalRead(SW) == LOW) {
+        if (millis() - pressStart > 5000) {
+          preferences.clear();
+          WiFi.softAPConfig(local_IP, gateway, subnet);
+          WiFi.softAP("SMART_TABLE");
+          server.on("/", handleRoot);
+          server.on("/save", handleSave);
+          server.begin();
+          serverMode = true;
+          break;
+        }
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+      }
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup() {
@@ -190,102 +174,40 @@ void setup() {
   pinMode(SW, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
 
-  encoder.setType(TYPE2);
-  encoder.setTickMode(AUTO);
-
   preferences.begin("wifi-config", false);
 
   savedSSID = preferences.getString("ssid", "");
   savedPassword = preferences.getString("password", "");
   savedMqttServer = preferences.getString("mqtt_server", "");
 
-  if (savedSSID != "" && savedPassword != "") {
-    connectToWiFi(savedSSID.c_str(), savedPassword.c_str());
-    if (savedMqttServer != "") { 
-      client.setServer(savedMqttServer.c_str(), mqtt_port);
-      client.setCallback(callback);
-      Serial.println("MQTT server setup complete.");
-    } else {
-      Serial.println("MQTT server not configured.");
-    }
-  } else {
-    serverMode = true;
-    IPAddress local_IP(192, 168, 4, 100);
-    IPAddress gateway(192, 168, 4, 100);
-    IPAddress subnet(255, 255, 255, 0);
+  WiFi.begin(savedSSID.c_str(), savedPassword.c_str());
+  client.setServer(savedMqttServer.c_str(), mqtt_port);
+  client.setCallback(callback);
 
+  encoder.setType(TYPE2);
+  encoder.setTickMode(AUTO);
+
+  if (savedSSID == "" || savedPassword == "") {
+    serverMode = true;
     WiFi.softAPConfig(local_IP, gateway, subnet);
     WiFi.softAP("SMART_TABLE");
     server.on("/", handleRoot);
     server.on("/save", handleSave);
     server.begin();
-
-    Serial.println("Access Point mode enabled.");
-    updateLED();
   }
-}
 
-
-void updateLED() {
-  static unsigned long lastBlinkTime = 0;
-  static int blinkCount = 0;
-
-  if (WiFi.status() != WL_CONNECTED) {
-    if (millis() - lastBlinkTime >= 500) {
-      lastBlinkTime = millis();
-      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-      blinkCount++;
-    }
-    if (blinkCount >= 2) {
-      blinkCount = 0;
-    }
-  } else if (!client.connected()) {
-    if (millis() - lastBlinkTime >= 200) {
-      lastBlinkTime = millis();
-      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-      blinkCount++;
-    }
-    if (blinkCount >= 4) {
-      blinkCount = 0;
-    }
-  } else {
-    digitalWrite(LED_PIN, HIGH);
-  }
-}
-
-void checkConnections() {
-  if (WiFi.status() != WL_CONNECTED) {
-    reconnectWiFi();
-  }
-  if (!client.connected()) {
-    client.unsubscribe(mqtt_topic_sub_mask);
-    reconnectMQTT();
-  }
+  xTaskCreate(TaskWiFiReconnect, "WiFi Reconnect", 2048, NULL, 1, NULL);
+  xTaskCreate(TaskMQTTReconnect, "MQTT Reconnect", 2048, NULL, 1, NULL);
+  xTaskCreate(TaskEncoder, "Encoder", 2048, NULL, 1, NULL);
+  xTaskCreate(TaskWebServer, "Web Server", 2048, NULL, 1, NULL);
+  xTaskCreate(TaskLED, "LED Control", 2048, NULL, 1, NULL);
+  xTaskCreate(TaskButton, "Button Control", 2048, NULL, 1, NULL);
 }
 
 void loop() {
-  if (serverMode) {
-    server.handleClient();
-    updateLED();
-    handleButtonPress();
-    return;
-  }
-
-  checkConnections();
-  updateLED();
-  client.loop();
-  encoder.tick();
-  if (encoder.isRight()) {
-    sendMqttCommand("increment/volume", "+");
-  }
-  if (encoder.isLeft()) {
-    sendMqttCommand("decrement/volume", "-");
-  }
-  if (encoder.isSingle()) {
-    sendMqttCommand("toggle/volume", "toggle");
-  }
-  handleButtonPress();
+  vTaskDelete(NULL);
 }
+
 
 
 // TP-Link_9F9A
